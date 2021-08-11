ab1fbc813e9d429e11d4a0616974277af512b428
advay-hub <70501638+advay-hub@users.noreply.github.com>
2021-08-11 08:17:05 +0530

    Update CameraRotate.cs

--- CameraRotate.cs
+++ CameraRotate.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class RotateCameraScript : MonoBehaviour
 {
     public float RotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
