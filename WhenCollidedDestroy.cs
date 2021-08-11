613caa97c48ae95cabe3dd8da530802fd17ca56a
advay-hub <70501638+advay-hub@users.noreply.github.com>
2021-08-11 08:21:11 +0530

    Update WhenCollidedDestroy.cs

--- WhenCollidedDestroy.cs
+++ WhenCollidedDestroy.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     private void OnTriggerEnter(collider other)
