51789849db378c17de3902f6717fe7f4d81aaa84
advay-hub <70501638+advay-hub@users.noreply.github.com>
2021-08-11 08:17:34 +0530

    Update DestroyOutOfBound.cs

--- DestroyOutOfBound.cs
+++ DestroyOutOfBound.cs
@@ -7,11 +7,7 @@ public class DestroyOutOfBound : MonoBehaviour
 {
     private float topBound  = 30;
     private float lowerBound = -10;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
