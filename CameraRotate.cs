98c6dcf7397fd30339f8eb608c4e09d092888267
advay-hub <70501638+advay-hub@users.noreply.github.com>
2021-08-11 08:25:01 +0530

    Update CameraRotate.cs

--- CameraRotate.cs
+++ CameraRotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCameraScript : MonoBehaviour
+public class CameraRotate : MonoBehaviour
 {
     public float RotationSpeed;
    
