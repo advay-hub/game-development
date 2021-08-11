6b1b8b1b630d317b6b26720dc04bededa7cf2019
advay-hub <70501638+advay-hub@users.noreply.github.com>
2021-08-11 08:18:28 +0530

    Update ObjectMovement.cs

--- ObjectMovement.cs
+++ ObjectMovement.cs
@@ -6,12 +6,7 @@ using UnityEngine;
 public class FoodMovement : MonoBehaviour
 {
   public float speed = 40.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
